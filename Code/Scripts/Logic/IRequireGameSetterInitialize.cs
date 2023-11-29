0fc335809a05cdb5b0094c473f7951300c71cde8
Kolyn090 <78561131+Kolyn090@users.noreply.github.com>
2023-11-29 10:27:31 -0500

    Update IRequireGameSetterInitialize.cs

--- Code/Scripts/Logic/IRequireGameSetterInitialize.cs
+++ Code/Scripts/Logic/IRequireGameSetterInitialize.cs
@@ -1,6 +1,11 @@
 using UnityEngine;
 
 
+/*
+    If a MonoBehaviour depends on gameSwtter,
+    It ought to implement this interface.
+    The detail of this idea is in gameSetter.cs
+*/
 namespace CS576.Janitor.Process
 {
     public interface IRequireGameSetterInitialize 
