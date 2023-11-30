558e3093f3525fe970ab4ae4a403c74a155d7a56
Kolyn090 <78561131+Kolyn090@users.noreply.github.com>
2023-11-30 10:26:16 -0500

    Update WaypointDedication.cs

--- Code/Scripts/VisualDebugging/WaypointDedication.cs
+++ Code/Scripts/VisualDebugging/WaypointDedication.cs
@@ -1,3 +1,10 @@
+/*
+    Also can be seen as the 'type' of a waypoint
+        City: waypoint spread across the city
+        Trashcan: records the position of a trashcan
+            swap point. Swap a trashcan on the position
+            in the beginning of the game
+*/
 namespace CS576.Janitor.Process
 {
     public enum WaypointDedication
