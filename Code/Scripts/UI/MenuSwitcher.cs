5c5d69411d28a896793c6666538314875824c812
Kolyn090 <78561131+Kolyn090@users.noreply.github.com>
2023-12-01 11:16:24 -0500

    Update MenuSwitcher.cs

--- Code/Scripts/UI/MenuSwitcher.cs
+++ Code/Scripts/UI/MenuSwitcher.cs
@@ -1,5 +1,11 @@
 using UnityEngine;
 
+
+/*
+    Used in the main scene
+    switch between the main UI compnents
+    and the configuration UI components
+*/
 namespace CS576.Janitor.UI
 {
     public class MenuSwitcher : MonoBehaviour
@@ -10,12 +16,15 @@ namespace CS576.Janitor.UI
         [SerializeField]
         private Canvas _configCanvas;
 
+        // used in onClick() of game configuration's 
+        // close button
         public void SwitchToMain()
         {
             _mainCanvas.gameObject.SetActive(true);
             _configCanvas.gameObject.SetActive(false);
         }
-
+        // used in onClick() of main's
+        // 'start' button
         public void SwtichToConfig()
         {
             _mainCanvas.gameObject.SetActive(false);
