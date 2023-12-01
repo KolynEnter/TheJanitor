ddaff0ed6f96456922c0c84e8c5a0d515fb2f9a0
Kolyn090 <78561131+Kolyn090@users.noreply.github.com>
2023-12-01 11:01:53 -0500

    Update GameMenuControllerInitializer.cs

--- Code/Scripts/UI/InGame/GameMenuControllerInitializer.cs
+++ Code/Scripts/UI/InGame/GameMenuControllerInitializer.cs
@@ -1,6 +1,11 @@
 using UnityEngine;
 
 
+/*
+    Help class that initializes the 
+        GameMenuController because SO
+        does not have a Start()
+*/
 namespace CS576.Janitor.UI
 {
     public class GameMenuControllerInitializer : MonoBehaviour
